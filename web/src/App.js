ef4c4c74862444ae5156cf465371db6d87afa799
Ivo Fritsch <averbeckfritsch@gmail.com>
2020-06-10 21:09:41 -0300

    Consumidor passa a exibir visualmente quando está esperando um produto

--- web/src/App.js
+++ web/src/App.js
@@ -4,11 +4,17 @@ import Paper from '@material-ui/core/Paper';
 import './App.css';
 
 
-function resolveStyleProduto(produto = '') {
+function resolveStyleProduto(produto = '', acao = '') {
+  const styleAcao = {}
+  if(acao.toLowerCase() === 'esperando'){
+    styleAcao.background = undefined
+    styleAcao.color = undefined
+  }
+
   switch(produto.toLowerCase()){
-    case "fanta": return { background: '#a63aae', color: 'white'}
-    case "coca": return { background: '#fe001a', color: 'white'}
-    default: return undefined
+    case "fanta": return { background: '#a63aae', color: 'white', border: '6px solid #a63aae', ...styleAcao}
+    case "coca": return { background: '#fe001a', color: 'white', border: '6px solid #fe001a', ...styleAcao}
+    default: return { border: '6px solid white'}
   }
 }
 
@@ -35,7 +41,7 @@ function App() {
           setProdutores(p => (
             {
               ...p, 
-              ...{ [payload.np]: payload.produto }
+              [payload.np]: payload.produto
             }
           ))
           break
@@ -43,7 +49,7 @@ function App() {
             setConsumidores(c => (
               {
                 ...c, 
-                ...{ [payload.nc]: { acao: 'Consumindo', produto:payload.produto} }
+                [payload.nc]: { acao: 'Consumindo', produto:payload.produto}
               }
             ))
             break
@@ -51,7 +57,7 @@ function App() {
               setConsumidores(c => (
                 {
                   ...c, 
-                  ...{ [payload.nc]: { acao: 'Esperando', produto:payload.produto} }
+                  [payload.nc]: { acao: 'Esperando', produto:payload.produto}
                 }
               ))
               break
@@ -72,7 +78,7 @@ function App() {
                   <Grid item xs={12}>
                     <Paper style={resolveStyleProduto(produtores[np])}>
                       <div style={{padding: '10px', textAlign: 'center', fontWeight: 'bold'}}>
-                        <h2>Produtor {np}</h2>
+                        <h2 style={{width: '170px'}}>Produtor {np}</h2>
                         <hr/>
                         {produtores[np] ? 
                         `Produzindo: ${produtores[np]}` :
@@ -85,13 +91,14 @@ function App() {
             )}
           </Grid>
           <Grid item>
-            <Paper>
+            <Paper style={{border: '6px solid black'}}>
               <div style={{padding: '10px', textAlign: 'center', fontWeight: 'bold'}}>
-                <h2>Estoque</h2>
+                <h2 style={{width: '170px'}}>Estoque</h2>
                 <hr/>
-                Qtd. Cocas: {estoque.QtdCoca}
+                <span style={{background: '#fe001a', color: 'white', padding: '3px'}}>Qtd. Cocas: {estoque.QtdCoca}</span>
                 <br/>
-                Qtd. Fantas: {estoque.QtdFanta}
+                <br/>
+                <span style={{background: '#a63aae', color: 'white', padding: '3px'}}>Qtd. Fantas: {estoque.QtdFanta}</span>
               </div>
             </Paper>
           </Grid>
@@ -99,9 +106,9 @@ function App() {
             {Object.keys(consumidores).map(nc => 
                 <React.Fragment key={nc}>
                   <Grid item xs={12}>
-                    <Paper style={resolveStyleProduto(consumidores[nc].produto)}>
+                    <Paper style={resolveStyleProduto(consumidores[nc].produto, consumidores[nc].acao)}>
                       <div style={{padding: '10px', textAlign: 'center', fontWeight: 'bold'}}>
-                        <h2>Consumidor {nc}</h2>
+                        <h2 style={{width: '170px'}}>Consumidor {nc}</h2>
                         <hr/>
                         {consumidores[nc].acao}: {consumidores[nc].produto}
                       </div>
